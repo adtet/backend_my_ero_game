75de30829f30aed5d145d0cedaeae08ec7d9ea03
Adiyatma Koto <adiyatma.padang@gmail.com>
2021-03-16 00:01:25 +0700

    adit commit itsuki-training step 1

--- dataset/itsuki/training_itsuki.py
+++ dataset/itsuki/training_itsuki.py
@@ -1,1 +1,28 @@
 import nltk
+
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+
+import json
+with open("itsuki.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['itsuki']:
+    for pola in intent['patterns']:
+        kata = nltk.word_tokenize(pola)
+        words.extend(kata)
+        docs_x.append(kata)
+        docs_y.append(intent['tag'])
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
